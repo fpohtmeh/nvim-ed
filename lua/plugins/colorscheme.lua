f5694e1abe3ce673c428f73a2ca9a01250ee6ef8
fpohtmeh <fpohtmehua@gmail.com>
2025-03-06 00:52:37 +0200

    Override some highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,19 @@ return {
   priority = 1000,
   opts = {
     style = "moon",
+    on_highlights = function(hl, c)
+      hl.LineNr = { fg = c.dark5 }
+      hl.LineNrAbove = { fg = c.dark5 }
+      hl.LineNrBelow = { fg = c.dark5 }
+      hl.CursorLineNr = { fg = c.green, bg = hl.CursorLine.bg }
+      hl.Comment = { fg = c.comment, italic = false }
+      hl.PmenuThumb = { bg = c.fg }
+      hl.WinSeparator = { fg = c.blue }
+
+      -- Snacks
+      hl.SnacksDashboardHeader = hl.ErrorMsg
+      hl.SnacksDashboardTitle = hl.Comment
+    end,
   },
   init = function()
     require("tokyonight").load()
