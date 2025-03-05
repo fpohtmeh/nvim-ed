de2503db0da4fdeb793f53b4ba5bdefbd60117af
fpohtmeh <fpohtmehua@gmail.com>
2025-03-06 00:03:24 +0200

    Add options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,40 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+local opt = vim.opt
+
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+opt.confirm = true
+opt.cursorline = true
+opt.expandtab = true
+opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+  diff = " ",
+  eob = " ",
+}
+opt.foldenable = false
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+opt.foldmethod = "expr"
+opt.list = true
+opt.number = true
+opt.relativenumber = true
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+opt.splitbelow = true
+opt.splitkeep = "screen"
+opt.splitright = true
+opt.smartindent = true
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.termguicolors = true
+opt.timeoutlen = 300
+opt.undofile = true
+opt.undolevels = 10000
+opt.updatetime = 200
+opt.virtualedit = "block"
+opt.winminwidth = 5
+opt.wrap = false
