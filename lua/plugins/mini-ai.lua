4ff77e2aa5b8fbb70e8ba26fd89896369c000ae8
fpohtmeh <fpohtmehua@gmail.com>
2025-03-06 17:35:25 +0200

    Add mini.ai plugin

---
+++ lua/plugins/mini-ai.lua
@@ -0,0 +1,19 @@
+return {
+  "echasnovski/mini.ai",
+  event = "VeryLazy",
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+        e = { -- Word with case
+          { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+          "^().*()$",
+        },
+      },
+    }
+  end,
+}
