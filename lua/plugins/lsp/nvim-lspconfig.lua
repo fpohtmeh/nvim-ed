36dd2e2900f3f146d58eb6587bcd4a2476cc3dbb
fpohtmeh <fpohtmehua@gmail.com>
2025-03-10 12:50:15 +0200

    Fix diagnostics window border

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,29 +1,28 @@
 local H = {}
 local icons = require("core.icons")
 
-H.opts = {
-  diagnostics = {
-    signs = {
-      text = {
-        [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
-        [vim.diagnostic.severity.WARN] = icons.diagnostics.warn,
-        [vim.diagnostic.severity.HINT] = icons.diagnostics.hint,
-        [vim.diagnostic.severity.INFO] = icons.diagnostics.info,
-      },
-    },
-  },
+H.signs_map = {
+  [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
+  [vim.diagnostic.severity.WARN] = icons.diagnostics.warn,
+  [vim.diagnostic.severity.HINT] = icons.diagnostics.hint,
+  [vim.diagnostic.severity.INFO] = icons.diagnostics.info,
 }
 
-H.configure_servers = function()
-  require("lspconfig").lua_ls.setup({})
-end
+H.diagnostic_opts = {
+  float = { border = "single" },
+}
 
 H.configure_diagnostic = function()
-  for severity, icon in pairs(H.opts.diagnostics.signs.text) do
+  for severity, icon in pairs(H.signs_map) do
     local name = vim.diagnostic.severity[severity]:lower():gsub("^%l", string.upper)
     name = "DiagnosticSign" .. name
     vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
   end
+  vim.diagnostic.config(H.diagnostic_opts)
+end
+
+H.configure_servers = function()
+  require("lspconfig").lua_ls.setup({})
 end
 
 return {
