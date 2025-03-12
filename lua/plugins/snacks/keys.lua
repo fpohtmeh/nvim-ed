c87c4c6abfa61a91a7b034a751f95ede7d6d0e38
fpohtmeh <fpohtmehua@gmail.com>
2025-03-13 01:43:14 +0200

    Add mapping for noice messages

--- lua/plugins/snacks/keys.lua
+++ lua/plugins/snacks/keys.lua
@@ -11,6 +11,7 @@ return {
   { "<leader>sb", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
   { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
   { "<leader>ss", function() Snacks.picker.git_status() end, desc = "Git Status" },
+  { "<leader>n", function() require("noice").cmd("all") end, desc = "Noice Messages" },
 
   { "<A-t>", function() Snacks.zen.zoom() end, desc = "Toggle Zen", mode = { "n", "x", "i" } },
   -- stylua: ignore end
