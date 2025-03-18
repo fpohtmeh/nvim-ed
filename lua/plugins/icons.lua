e72e0e1c996a5463b413d483952dcc3ba50fa5b9
fpohtmeh <fpohtmehua@gmail.com>
2025-03-18 14:54:07 +0200

    Fix some general icons

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,4 +1,23 @@
+vim.filetype.add({
+  extension = {
+    plist = "xml",
+    manifest = "xml",
+    log = "log",
+  },
+})
+
 return {
   "nvim-tree/nvim-web-devicons",
   event = "LazyFile",
+  opts = {
+    override_by_extension = {
+      dll = { icon = "", color = "#FF0500", cterm_color = "52", name = "Dll" },
+      exe = { icon = "", color = "#FF0500", cterm_color = "124", name = "Exe" },
+      plist = { icon = "󰗀", color = "#E37933", cterm_color = "166", name = "Plist" },
+      manifest = { icon = "󰗀", color = "#E37933", cterm_color = "166", name = "Manifest" },
+    },
+    override_by_filename = {
+      ["CMakeLists.txt"] = { icon = "", color = "#DCE3EB", cterm_color = "254", name = "CMake" },
+    },
+  },
 }
