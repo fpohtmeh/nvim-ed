e04a40cede87f4483ef335ea2873df84a3d11e48
fpohtmeh <fpohtmehua@gmail.com>
2025-11-27 18:26:28 +0200

    Update C++ parser in overseer

--- lua/plugins/overseer/parsers/cpp.lua
+++ lua/plugins/overseer/parsers/cpp.lua
@@ -11,6 +11,7 @@ local r = {
   begin = "^",
   filename = "(.-)",
   line_col = "%((%d+),?(%d*)%)",
+  line_col_colon = ":(%d+):(%d+)",
   type = "(%a*%s?error)",
   code = "(%w%d+)",
   any_text = "(.+)",
@@ -20,16 +21,30 @@ local r = {
 -- Examples:
 -- * D:\Projects\foo\bar.cpp(2,5): error C1234: '=': left operand must be l-value [D:\Projects\foo\build\bar.vcxproj]
 -- * ..\foo\main.cpp(31): error C4430: missing type specifier - int assumed. Note: C++ does not support default-int
+-- * /Users/user/Projects/MyApp/MyRenderer.cpp:29:16: error: no member named 'property' in 'MyClass'
 M.node = {
-  "extract",
-  { postprocess = fix_filename },
-  r.begin .. r.filename .. r.line_col .. ": " .. r.type .. " " .. r.code .. ": " .. r.any_text .. r.eol,
-  "filename",
-  "lnum",
-  "col",
-  "type",
-  "code",
-  "text",
+  "loop",
+  {
+    "extract",
+    { postprocess = fix_filename },
+    r.begin .. r.filename .. r.line_col .. ": " .. r.type .. " " .. r.code .. ": " .. r.any_text .. r.eol,
+    "filename",
+    "lnum",
+    "col",
+    "type",
+    "code",
+    "text",
+  },
+  {
+    "extract",
+    { postprocess = fix_filename },
+    r.begin .. r.filename .. r.line_col_colon .. ": " .. r.type .. ": " .. r.any_text .. r.eol,
+    "filename",
+    "lnum",
+    "col",
+    "type",
+    "text",
+  },
 }
 
 return M
