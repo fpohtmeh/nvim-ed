4d5d6bc161b864c5507d0203ffece3fd2e039fe2
fpohtmeh <fpohtmehua@gmail.com>
2026-02-15 20:21:27 +0200

    Add textobjects for next closing bracket, next quotation mark, etc.

---
+++ lua/plugins/text-objects.lua
@@ -0,0 +1,17 @@
+return {
+  "chrisgrieser/nvim-various-textobjs",
+  event = "LazyFile",
+  opts = {
+    forwardLooking = { big = 30 },
+    keymaps = { useDefaults = false },
+  },
+  keys = {
+    -- stylua: ignore start
+      { mode = { "o", "x" }, ";i", function() require("various-textobjs").restOfIndentation() end, desc = "Rest of indentation"  },
+    { mode = { "o", "x" }, ";i", function() require("various-textobjs").restOfIndentation() end, desc = "Rest of indentation"  },
+    { mode = { "o", "x" }, ";p", function() require("various-textobjs").restOfParagraph() end, desc = "Rest of paragraph"  },
+    { mode = { "o", "x" }, ";b", function() require("various-textobjs").toNextClosingBracket() end, desc = "Next closing bracket"  },
+    { mode = { "o", "x" }, ";q", function() require("various-textobjs").toNextQuotationMark() end, desc = "Next quotation mark" },
+    -- stylua: ignore end
+  },
+}
