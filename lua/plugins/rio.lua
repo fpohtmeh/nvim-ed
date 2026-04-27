4c754ca1fa9caec7e8fa82e97e6636b2436e6f47
fpohtmeh <fpohtmehua@gmail.com>
2026-04-27 18:28:15 +0300

    Add CR keymap for fugitive commit view

--- lua/plugins/rio.lua
+++ lua/plugins/rio.lua
@@ -1,3 +1,8 @@
+local function commit_hash_under_cursor()
+  local line = vim.api.nvim_get_current_line()
+  return line:match("^(%x+)")
+end
+
 local function git_log()
   require("rio").run("git log -100 --oneline --no-merges", {
     callbacks = {
@@ -6,6 +11,14 @@ local function git_log()
         table.insert(callbacks, function(handle)
           vim.bo[handle.buf].filetype = "git"
         end)
+      end,
+    },
+    keys = {
+      ["<CR>"] = function()
+        local hash = commit_hash_under_cursor()
+        if hash then
+          vim.cmd("0Git show " .. hash)
+        end
       end,
     },
   })
