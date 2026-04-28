7137a08174c481c75fd215a76d11bff43859fd22
fpohtmeh <fpohtmehua@gmail.com>
2026-04-29 01:50:31 +0300

    Handle any cursor position in commit block

--- lua/plugins/rio/git.lua
+++ lua/plugins/rio/git.lua
@@ -2,7 +2,18 @@ local M = {}
 
 M.commit_hash_under_cursor = function()
   local line = vim.api.nvim_get_current_line()
-  return line:match("^(%x+)")
+  local hash = line:match("^commit (%x+)") or line:match("^(%x%x%x%x%x%x%x+)")
+  if hash then
+    return hash
+  end
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  for i = row - 1, 1, -1 do
+    line = vim.api.nvim_buf_get_lines(0, i - 1, i, false)[1]
+    hash = line:match("^commit (%x+)")
+    if hash then
+      return hash
+    end
+  end
 end
 
 M.file_under_cursor = function()
