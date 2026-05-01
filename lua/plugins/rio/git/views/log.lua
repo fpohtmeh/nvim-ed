7391fcb4d1b82124ced2ec0db608dfbb0ac087c9
fpohtmeh <fpohtmehua@gmail.com>
2026-05-01 19:40:30 +0300

    Improve git file log view

--- lua/plugins/rio/git/views/log.lua
+++ lua/plugins/rio/git/views/log.lua
@@ -1,17 +1,20 @@
 local H = {}
 
 local builtin = require("rio.callbacks.builtin")
+local rio = require("rio")
 local togglers = require("rio.togglers")
+local diff = require("plugins.rio.git.views.diff")
+local parse = require("plugins.rio.git.parse")
 local util = require("plugins.rio.git.util")
 
 ---@type Rio.KeyDef
 H.open_commit_diff = {
   fn = function(handle)
-    local hash = require("plugins.rio.git.parse").commit_hash_under_cursor(handle)
+    local hash = parse.commit_hash_under_cursor(handle)
     if not hash then
       return
     end
-    require("plugins.rio.git.views.diff").commit(hash, handle.state)
+    diff.commit(hash, handle.state)
   end,
   desc = "open diff",
   group = "Navigate",
@@ -28,6 +31,74 @@ H.reset_last_commit = {
   desc = "reset last commit",
   group = "Reset",
 }
+
+H.save_file_state = function(parent, cursor)
+  return function(handle)
+    parent.state.file_buf = handle.state.buf
+    parent.state.file_win = handle.state.win
+    if not cursor then
+      return
+    end
+    local max = vim.api.nvim_buf_line_count(handle.state.buf)
+    vim.api.nvim_win_set_cursor(handle.state.win, { math.min(cursor[1], max), cursor[2] })
+  end
+end
+
+---@param file string
+---@return Rio.KeyDef
+H.show_file_at_commit = function(file)
+  local ft = vim.filetype.match({ filename = file }) or ""
+  return {
+    fn = function(handle)
+      local hash = parse.commit_hash_under_cursor(handle)
+      if not hash then
+        return
+      end
+      local win = handle.state.file_win
+      local cursor = win and vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_cursor(win)
+      local cmd = "git show " .. hash .. ":" .. file
+      rio.run(cmd, {
+        state = { buf = handle.state.file_buf, win = handle.state.file_win },
+        callbacks = {
+          on_finish = {
+            builtin.set_filetype(ft),
+            H.save_file_state(handle, cursor),
+          },
+        },
+      })
+    end,
+    desc = "show file at commit",
+    group = "Navigate",
+  }
+end
+
+---@param file string
+---@return Rio.KeyDef
+H.show_file_diff_at_commit = function(file)
+  return {
+    fn = function(handle)
+      local hash = parse.commit_hash_under_cursor(handle)
+      if not hash then
+        return
+      end
+      local cmd = "git diff " .. hash .. "~1 " .. hash .. " -- " .. file
+      rio.run(cmd, {
+        state = { buf = handle.state.file_buf, win = handle.state.file_win },
+        callbacks = {
+          on_finish = {
+            builtin.set_filetype("diff"),
+            function(inner)
+              handle.state.file_buf = inner.state.buf
+              handle.state.file_win = inner.state.win
+            end,
+          },
+        },
+      })
+    end,
+    desc = "show file diff at commit",
+    group = "Navigate",
+  }
+end
 
 ---@type Rio.KeyDef
 H.pull = {
@@ -51,9 +122,9 @@ H.push = {
 return function(opts)
   opts = opts or {}
   local oneline = opts.oneline ~= false
-  local file = opts.file and vim.fn.expand("%:p") or nil
+  local file = opts.file and vim.fn.expand("%:.") or nil
   local cmd = "git log {limit} {oneline} {merges} {decorate}" .. (file and " -- " .. file or "")
-  require("rio").run(cmd, {
+  rio.run(cmd, {
     callbacks = {
       on_finish = { builtin.set_filetype("git") },
     },
@@ -66,6 +137,8 @@ return function(opts)
     keys = {
       ["<CR>"] = H.open_commit_diff,
       R = H.reset_last_commit,
+      o = file and H.show_file_at_commit(file) or false,
+      d = file and H.show_file_diff_at_commit(file) or false,
       p = H.pull,
       P = H.push,
       tl = togglers.key("limit"),
