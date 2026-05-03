aecc3be334ad952226435a3e86ff590fd82c78e8
fpohtmeh <fpohtmehua@gmail.com>
2026-05-03 10:59:00 +0300

    Use rio link builtin for cursor preserve

--- lua/plugins/rio/git/views/log/file.lua
+++ lua/plugins/rio/git/views/log/file.lua
@@ -1,29 +1,9 @@
 local M = {}
 
 local builtin = require("rio.callbacks.builtin")
+local link_builtin = require("rio.link.builtin")
 local rio = require("rio")
 local parse = require("plugins.rio.git.parse")
-
-local H = {}
-
-H.linked_cursor = function(handle, key)
-  local linked = handle.state.links and handle.state.links[key]
-  if not linked then
-    return nil
-  end
-  local win = linked.state.win
-  if not win or not vim.api.nvim_win_is_valid(win) then
-    return nil
-  end
-  return vim.api.nvim_win_get_cursor(win)
-end
-
-H.restore_cursor = function(cursor)
-  return function(handle)
-    local max = vim.api.nvim_buf_line_count(handle.state.buf)
-    vim.api.nvim_win_set_cursor(handle.state.win, { math.min(cursor[1], max), cursor[2] })
-  end
-end
 
 ---@param file string
 ---@return Rio.KeyDef
@@ -35,14 +15,9 @@ M.show_at_commit = function(file)
       if not hash then
         return
       end
-      local cursor = H.linked_cursor(handle, "file")
-      local on_finish = { builtin.set_filetype(ft) }
-      if cursor then
-        table.insert(on_finish, H.restore_cursor(cursor))
-      end
       rio.run("git show " .. hash .. ":" .. file, {
-        link = "file",
-        callbacks = { on_finish = on_finish },
+        link = link_builtin.preserve_cursor("file"),
+        callbacks = { on_finish = { builtin.set_filetype(ft) } },
       })
     end,
     desc = "show file at commit",
@@ -61,7 +36,7 @@ M.show_diff_at_commit = function(file)
       end
       local cmd = "git diff " .. hash .. "~1 " .. hash .. " -- " .. file
       rio.run(cmd, {
-        link = "file",
+        link = { key = "file" },
         callbacks = {
           on_finish = { builtin.set_filetype("diff") },
         },
