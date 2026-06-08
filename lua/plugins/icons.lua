153522f2b7b5c61493f03cc4b43435fc371458c6
fpohtmeh <fpohtmehua@gmail.com>
2026-06-08 21:38:44 +0300

    Add yml icon override

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -7,6 +7,7 @@ return {
       exe = { icon = "", color = "#FF0500", cterm_color = "124", name = "Exe" },
       plist = { icon = "󰗀", color = "#E37933", cterm_color = "166", name = "Plist" },
       manifest = { icon = "󰗀", color = "#E37933", cterm_color = "166", name = "Manifest" },
+      yml = { icon = "󰗀", color = "#D70000", cterm_color = "160", name = "Yaml" },
     },
     override_by_filename = {
       ["CMakeLists.txt"] = { icon = "", color = "#DCE3EB", cterm_color = "254", name = "CMake" },
