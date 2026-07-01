7aa1c40e2b708f01620bcb759254358e85d4a5f7
fpohtmeh <fpohtmehua@gmail.com>
2026-07-02 02:44:51 +0300

    Use context module in clipboard

--- lua/core/clipboard.lua
+++ lua/core/clipboard.lua
@@ -1,6 +1,8 @@
 local H = {}
 local M = {}
 
+local context = require("core.context")
+
 H.yank = function(value)
   vim.fn.setreg("+", value)
   Snacks.notify.info("Copied: " .. value)
@@ -11,57 +13,17 @@ H.git_output = function(args)
   return vim.v.shell_error == 0 and result[1] or nil
 end
 
-H.copy = {
-  file = {
-    full_path = function(p)
-      return p
-    end,
-    dir_path = function(p)
-      return vim.fn.fnamemodify(p, ":h") .. "/"
-    end,
-    filename = function(p)
-      return vim.fn.fnamemodify(p, ":t")
-    end,
-    relative_path = function(p)
-      return vim.fn.fnamemodify(p, ":.")
-    end,
-  },
-  oil = {
-    full_path = function(dir, entry)
-      return dir .. (entry and entry.name or "")
-    end,
-    dir_path = function(dir, _)
-      return dir
-    end,
-    filename = function(_, entry)
-      return entry and entry.name or ""
-    end,
-    relative_path = function(dir, entry)
-      return vim.fn.fnamemodify(dir .. (entry and entry.name or ""), ":.")
-    end,
-  },
-}
-
-H.resolve = function(name)
-  if vim.bo.filetype == "oil" then
-    local oil = require("oil")
-    local dir = require("core.fs").to_unix(oil.get_current_dir())
-    return H.copy.oil[name](dir, oil.get_cursor_entry())
-  end
-  return H.copy.file[name](vim.fn.expand("%:p"))
-end
-
 M.yank_filepath = function()
-  H.yank(H.resolve("full_path"))
+  H.yank(context.current.full_path)
 end
 M.yank_filename = function()
-  H.yank(H.resolve("filename"))
+  H.yank(context.current.filename)
 end
 M.yank_relative_path = function()
-  H.yank(H.resolve("relative_path"))
+  H.yank(context.current.relative_path)
 end
 M.yank_directory = function()
-  H.yank(H.resolve("dir_path"))
+  H.yank(context.current.dir_path)
 end
 M.yank_git_branch = function()
   H.yank(H.git_output("git branch --show-current") or "not a git repo")
